345bbc3b65c485c8e70116368c4f670acbaa742d
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-10-01 23:09:01 +0530

    using form data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,25 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  userInformation = {
+    userName: '',
+    emailAddress: '',
+    secretQuestion: '',
+    questionAnswer: '',
+    gender: '',
+  };
+  submitted = false;
 
   onSubmitForm() {
+    this.submitted = true;
     //console.log('Form Submitted!!', form);
     console.log(this.formElement);
+    this.userInformation.userName = this.formElement.value.userData.usernameField;
+    this.userInformation.emailAddress = this.formElement.value.userData.emailField;
+    this.userInformation.secretQuestion = this.formElement.value.secretField;
+    this.userInformation.questionAnswer = this.formElement.value.qAField;
+    this.userInformation.gender = this.formElement.value.genderField;
+    console.log(this.userInformation.userName);
   }
 
   onSuggestUserName() {
